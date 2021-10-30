98a314be923a34f05ec3e1b7867f0119afdabac5
AlanTheKnight <51376126+AlanTheKnight@users.noreply.github.com>
2021-10-30 10:29:44 +0300

    Improve deploy script

--- deploy.mjs
+++ deploy.mjs
@@ -1,18 +1,17 @@
 import execa from "execa";
-import * as fs from "fs";
 
 async function deploy() {
   try {
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
     console.log("Building started...");
-    await execa("", ["run", "build"]);
-    const folderName = fs.existsSync("dist") ? "dist" : "build";
+    await execa("yarn", ["run", "build"]);
+    const folderName = "dist";
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
     await execa("rm", ["-r", folderName]);
-    await execa("git", ["checkout", "-f", "master"]);
+    await execa("git", ["checkout", "-f", "main"]);
     await execa("git", ["branch", "-D", "gh-pages"]);
     console.log("Successfully deployed, check your settings");
   } catch (e) {
